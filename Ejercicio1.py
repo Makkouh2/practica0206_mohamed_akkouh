696da4feb1d75cd119f119f55b393d4f1331a7bc
Xsabalzerr <113203972+Xabijaress@users.noreply.github.com>
2023-12-14 16:07:52 +0100

    Ejercicio corregido

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,8 +1,21 @@
-
 n=int(input("Escriba un número entero entre el 1 y 10: "))
-file="tabla"+ str(n)+ ".txt"
-m=open(file,"w")
-for i in range (1,11):
-    x=n*i
-    print(x)
-    m.write(str(x) + "\n")
+def multipicacion(n):
+    """Función para relizar una multiplicación
+
+    parametros:
+    -n=numero entero
+
+    salida:
+    -tabla de la multiplicación de dicho numero.
+    """
+if n<=10 and n>0:
+    file="tabla"+ str(n)+ ".txt"
+    m=open(file,"w")
+    for i in range (1,11):
+        x=n*i
+        print(x)
+        m.write(str(x) + "\n")
+else:
+    print("No se cumple")
+    
+multipicacion (n)
